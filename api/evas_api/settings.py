bfa1a36d1c731f730cabbc914d52c51d5ddf61dc
Kevin <etienne.kevin@gmail.com>
2014-07-20 00:05:44 +0100

    Add token auth and session

--- api/evas_api/settings.py
+++ api/evas_api/settings.py
@@ -43,6 +43,13 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
